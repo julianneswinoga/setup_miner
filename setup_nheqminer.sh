8109af7d3e4a30eabf9857ae413b88e036c6d09e
Cameron Swinoga <cameronswinoga@gmail.com>
2017-10-25 12:43:44 -0400

    Added zogminer

--- setup_nheqminer.sh
+++ setup_nheqminer.sh
@@ -19,12 +19,32 @@ done
 echo Package manager: $apt
 
 if [ $apt = "yum" ] || [ $apt = "dnf" ]; then
+    # nheqminer
     sudo $apt install -y git cmake @development-tools boost-devel "gcc-c++"
+    # zogminer
+    sudo $apt install -y \
+        git pkgconfig automake autoconf ncurses-devel python wget vim-common \
+        gtest-devel gcc "gcc-c++" libtool patch opencl-headers mesa-libGL-devel
 else
+    # nheqminer
     sudo $apt install -y git cmake build-essential libboost-all-dev
-    #sudo apt-get install -y cmake pkg-config python ocl-icd-dev libegl1-mesa-dev ocl-icd-opencl-dev libdrm-dev libxfixes-dev libxext-dev llvm-3.6-dev clang-3.6 libclang-3.6-dev libtinfo-dev libedit-dev zlib1g-dev
+    # beignet
+    sudo $apt install -y \
+        cmake pkg-config python ocl-icd-dev libegl1-mesa-dev ocl-icd-opencl-dev \
+        libdrm-dev libxfixes-dev libxext-dev "llvm-3.6-dev" "clang-3.6" \
+        "libclang-3.6-dev" libtinfo-dev libedit-dev zlib1g-dev
+    # zogminer
+    sudo $apt install \
+        build-essential pkg-config libc6-dev m4 "g++-multilib" \
+        autoconf libtool ncurses-dev unzip git python \
+        zlib1g-dev wget bsdmainutils automake opencl-headers \
+        mesa-common-dev
 fi
 
+mkdir setup_miner
+cd setup_miner
+
+# nheqminer setup
 rm -rf ./nheqminer
 git clone -b Linux https://github.com/nicehash/nheqminer.git
 cd nheqminer/cpu_xenoncat/Linux/asm/
@@ -32,11 +52,26 @@ sh ./assemble.sh
 cd ../../../Linux_cmake/nheqminer_cpu
 cmake .
 make -j $(nproc)
-cd ../../
+cd ../../../
+
+# zogminer setup
+rm -rf ./zogminer
+git clone https://github.com/nginnever/zogminer.git
+cd zogminer
+./zcutil/fetch-params.sh
+./zcutil/build.sh -j$(nproc)
+cd ../
 
 cat > start.sh <<- EOM
 #!/bin/sh
-./Linux_cmake/nheqminer_cpu/nheqminer_cpu -l us1-zcash.flypool.org:3333 -u t1KBPmuei8cKRKCXPUtLhXyuNmkVqd9sK1X -t $(nproc)
+
+trap "trap - TERM && kill -- -$$" INT TERM EXIT
+
+./nheqminer/Linux_cmake/nheqminer_cpu/nheqminer_cpu -l us1-zcash.flypool.org:3333 -u t1KBPmuei8cKRKCXPUtLhXyuNmkVqd9sK1X -t $(nproc) &
+
+./zogminer/src/zcash-miner -G -stratum="stratum+tcp://us1-zcash.flypool.org:3333" -user=t1KBPmuei8cKRKCXPUtLhXyuNmkVqd9sK1X &
+
+read
 EOM
 
 chmod +x start.sh
