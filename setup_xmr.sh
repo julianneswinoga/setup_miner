8a15265d3230fb5ca2ebcefd59c3685903a93c98
Julianne Swinoga <julianneswinoga@gmail.com>
2017-10-30 14:47:27 -0400

    Added email

--- setup_xmr.sh
+++ setup_xmr.sh
@@ -54,7 +54,7 @@ $(for (( i=1; i<=$(nproc); i++ )); do echo '{ "low_power_mode" : false, "no_pref
 "tls_secure_algo" : true,
 "tls_fingerprint" : "",
 "pool_address" : "xmr-us-east1.nanopool.org:14444",
-"wallet_address" : "4ApJyxddzmmQ383MUX6QiMXFVBWjn3q9u3uPbh3d3NVfPSKgvSYFM1JVfK8rJqUwA4Td3rjnYd3ZA6dRUYLVzzoQN1nDZ76.default",
+"wallet_address" : "4ApJyxddzmmQ383MUX6QiMXFVBWjn3q9u3uPbh3d3NVfPSKgvSYFM1JVfK8rJqUwA4Td3rjnYd3ZA6dRUYLVzzoQN1nDZ76.default/camca96@gmail.com",
 "pool_password" : "x",
 "call_timeout" : 10,
 "retry_time" : 10,
